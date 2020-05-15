ab3d7744341daba07a6f6163550d63ea5427a710
Mladen Rosić <mladenro80@gmail.com>
2020-05-15 19:30:54 +0200

    NgIf i NgFor

--- first-project/src/app/date/date.component.ts
+++ first-project/src/app/date/date.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Student } from '../model/Student';
 
 @Component({
@@ -11,6 +11,9 @@ export class DateComponent implements OnInit {
   currentDate:String;
   currentStudent:Student;
 
+  @Input('myTitle')
+  mojNaslov:String;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,19 +23,35 @@ export class DateComponent implements OnInit {
       this.currentDate = tempDate.toDateString()+" "+tempDate.toLocaleTimeString();
     }, 1000);
 
+
+
     this.currentStudent = new Student();
 
     this.currentStudent.brIndexa=1;
     this.currentStudent.ime="Mladen";
     this.currentStudent.prezime = "Rosić";
-    this.currentStudent.tel.push("065-111-222");
+/*     this.currentStudent.tel.push("065-111-222");
+    this.currentStudent.tel.push("055-555-666"); */
     console.log("Current student: ");
     console.log(this.currentStudent);
+
+    for(let item of this.currentStudent.tel){
+      console.log(item);
+    }
+
+    let i;
+    for(i=0; i<this.currentStudent.tel.length; i++){
+      console.log(this.currentStudent.tel[i]);
+    }
 
   }
 
   public saberiDvaBroja(x:number, y:number): number{
     return x+y;
+  }
+
+  public logName(){
+    console.log(this.currentStudent.ime);
   }
 
 }
