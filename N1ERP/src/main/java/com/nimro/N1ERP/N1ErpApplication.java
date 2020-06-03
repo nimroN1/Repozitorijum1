597020c6cd06db2fa335f4b95676e1013aa5681f
Mladen Rosić <mladenro80@gmail.com>
2020-06-03 19:40:57 +0200

    Project started

---
+++ N1ERP/src/main/java/com/nimro/N1ERP/N1ErpApplication.java
@@ -0,0 +1,13 @@
+package com.nimro.N1ERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N1ErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N1ErpApplication.class, args);
+	}
+
+}
