0f92fb3cc188180b63b84a9df9d355c9dda72f4e
Mladen Rosić <mladenro80@gmail.com>
2020-06-29 18:04:53 +0200

    CorsConfiguration added

--- N1ERP/src/main/java/com/nimro/N1ERP/N1ErpApplication.java
+++ N1ERP/src/main/java/com/nimro/N1ERP/N1ErpApplication.java
@@ -2,6 +2,11 @@ package com.nimro.N1ERP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class N1ErpApplication {
@@ -9,5 +14,15 @@ public class N1ErpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(N1ErpApplication.class, args);
 	}
+	
+	/*
+	 * @Bean public CorsFilter corsFilter() { UrlBasedCorsConfigurationSource source
+	 * = new UrlBasedCorsConfigurationSource(); CorsConfiguration config = new
+	 * CorsConfiguration(); config.setAllowCredentials(true);
+	 * config.addAllowedOrigin("*"); config.addAllowedHeader("*");
+	 * config.addExposedHeader("total-pages"); config.addAllowedMethod("*");
+	 * source.registerCorsConfiguration("/**", config); return new
+	 * CorsFilter((CorsConfigurationSource) source); }
+	 */
 
 }
